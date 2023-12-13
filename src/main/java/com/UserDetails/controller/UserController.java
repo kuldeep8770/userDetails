546bc922e6afc4a4eacce72730f920d2c5726594
kuldeep sharma <kuldeepsharma30611@gmail.com>
2023-12-13 12:53:23 +0530

    user details

---
+++ src/main/java/com/UserDetails/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.UserDetails.controller;
+
+import com.UserDetails.entity.User;
+import com.UserDetails.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllusers(@RequestBody User user){
+        return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/UserDetails/service/UserService.java
@@ -0,0 +1,24 @@
+package com.UserDetails.service;
+
+import com.UserDetails.entity.User;
+import com.UserDetails.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user){
+        return userRepository.save(user);
+
+            }
+
+    public List<User> getAllUsers(){
+         return userRepository.findAll();
+
+    }
+}
